5c35d29155a08d3df08fcd5a43d72aa8f912c416
Kachyz <kachiiz@gmail.com>
2019-02-11 21:41:55 -0600

    adding cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 let app = express()
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const rutaLibros = require('./routes/libros')
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/libreria')
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use('/libros', rutaLibros)
 
